26ba24c0c3ea5fa0a670231807ccc8ed4d498815
zrielp <144454643+zrielp@users.noreply.github.com>
2025-08-03 20:30:35 +0700

    Jenis-jenis Pengulangan

---
+++ Control Flow/Pengulangan (for, if, while)/Latihan/taxi.js
@@ -0,0 +1,9 @@
+// for (var i = 0; i < 10; i++) {
+//   console.log('Taxi No. ' + i + ' berangkat dengan lancar');
+// }
+var totalTaxi = 10;
+var taxi = 1;
+while (taxi <= totalTaxi) {
+  console.log('Taxi No. ' + taxi + ' berangkat dengan lancar.');
+  taxi++;
+}
