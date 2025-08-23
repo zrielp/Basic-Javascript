bdaabb6d0a24968bd68878c574549b9f2ebed022
zrielp <144454643+zrielp@users.noreply.github.com>
2025-08-23 23:43:03 +0700

    Update latihan 4

--- Control Flow/Pengulangan (for, if, while)/Latihan/taxi.js
+++ Control Flow/Pengulangan (for, if, while)/Latihan/taxi.js
@@ -1,7 +1,9 @@
+// latihan 1
 // for (var i = 0; i < 10; i++) {
 //   console.log('Taxi No. ' + i + ' berangkat dengan lancar');
 // }
 
+// latihan 2
 var totalTaxi = 10;
 var taxiBeroperasi = 6;
 var noTaxi = 1;
@@ -10,9 +12,14 @@ var noTaxi = 1;
 //   noTaxi++;
 // }
 
+// latihan 3
 for (noTaxi; noTaxi <= totalTaxi; noTaxi++) {
-  if (noTaxi < 7) {
+  if (noTaxi <= 6) {
     console.log('Taxi No. ' + noTaxi + ' berangkat dengan lancar.');
+    // latihan 4
+  } else if (noTaxi === 8) {
+    console.log('Taxi No. ' + noTaxi + ' sedang dalam lembur.');
+    //
   } else {
     console.log('Taxi No. ' + noTaxi + ' tidak siap untuk berangkat.');
   }
